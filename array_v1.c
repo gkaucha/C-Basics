09a6552dad8ff489a146893d85217610a56f7222
avadhuta03 <gagboy917@gmail.com>
2019-03-21 21:46:43 +0000

    adds array_v1.c

---
+++ array_v1.c
@@ -0,0 +1,25 @@
+//count digits, white spaces, others //
+#include<stdio.h>
+int main()
+{
+    int c, i, whiteC, otherC;
+    int ndigit[10];
+
+    whiteC = otherC = 0;
+    for( i=0; i<10; ++i)
+        ndigit[i] = 0;
+
+    while((c=getchar()) != EOF){
+         if(c >= '0' && c <= '9')
+            { ++ndigit[c-'0']; }
+         else if (c ==' ' || c == '\n' || c == '\t')
+            { ++whiteC; }
+         else
+            { ++otherC; }
+    }  
+    printf("\ndigits : ");
+    for(i=0; i<10; ++i)
+         printf("%d", ndigit[i]);
+    printf("\n white spaces = %d \n other Characters = %d\n", whiteC, otherC); 
+
+}
